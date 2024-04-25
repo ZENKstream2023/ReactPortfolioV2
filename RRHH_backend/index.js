d2284c5616835fc07b3610c696ca2d88382e6134
ZENKstream2023 <zenkstreamhelp@gmail.com>
2024-04-25 17:26:59 +0200

    modified:   RRHH_backend/index.js 	modified:   React_FrontendV2/src/Global.js 	new file:   React_FrontendV2/src/data/sucursales.json

--- RRHH_backend/index.js
+++ RRHH_backend/index.js
@@ -1,102 +1,89 @@
 "use strict";
-// Definir constantes
-const express = require("express");
 const path = require("path");
+const express = require("express");
 const cookieParser = require("cookie-parser");
-const https = require("https"); // Importa el módulo HTTPS
+const http = require("http");
+const https = require("https");
 const fs = require("fs");
-const helmet = require("helmet");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const mongoose = require("mongoose");
-require("dotenv").config();
-const app = express();
-const port = process.env.PORT || 3200;
-const routes = require("./routes/routes");
-const User = require("./models/user");
-app.use(bodyParser.json());
-app.use(cookieParser());
-app.use(helmet());
 const options = {
 	key: fs.readFileSync("localhost.key"), // Lee el archivo de clave privada
 	cert: fs.readFileSync("localhost.crt"), // Lee el archivo de certificado
 };
+const helmet = require("helmet");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 3200;
+const routes = require("./routes/routes");
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(helmet());
 const { verifyToken } = require("./security/authMiddleware"); // Importa el middleware de autenticación
 
 // Middleware de autenticación global
 app.use(verifyToken);
+// Middleware para redirigir el tráfico HTTP a HTTPS
+app.use((req, res, next) => {
+    if (req.secure) {
+        next();
+    } else {
+        res.redirect('https://' + req.headers.host + req.url);
+    }
+});
 // CORS
+//app.use(cors());
 const corsOptions = {
-	origin: "https://localhost:3200",
-	methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-	credentials: true,
-	optionsSuccessStatus: 204,
+        origin: "https://localhost:3200",
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+        credentials: true,
+
+        optionsSuccessStatus: 204,
 };
 
 app.use(cors(corsOptions));
 
-// Content Security Policy
-app.use((req, res, next) => {
-	res.setHeader(
-		"Content-Security-Policy",
-		"script-src 'self' https://cdn.jsdelivr.net/; img-src 'self' http://localhost:3200;",
-	);
-	next();
-});
-
-// Sirve el frontend estático desde /var/www/frontend/dist
+// Sirve el frontend estático desde /var/www/frontend/distI
 app.use(express.static(path.join(__dirname, "../Vue_FrontendV2/dist")));
-// Middleware para servir archivos CSS con el tipo MIME correcto
-app.use(
-	"/assets/css",
-	(req, res, next) => {
-		res.setHeader("Content-Type", "text/css");
-		next();
-	},
-	express.static(path.join(__dirname, "assets/css")),
-);
-
-// Middleware para servir archivos JS con el tipo MIME correcto
-app.use(
-	"/assets/js",
-	(req, res, next) => {
-		res.setHeader("Content-Type", "text/javascript");
-		next();
-	},
-	express.static(path.join(__dirname, "assets/js")),
-);
+// Definir la política CSP
+app.use((req, res, next) => {
+  res.setHeader('Content-Security-Policy', "img-src 'self' https://*.jtvnw.net https://localhost:3200/*");
+  next();
+});
 
 // Manejar todas las demás rutas y redirigirlas al archivo de entrada del frontend
 // Middleware para excluir rutas que comiencen por "/api"
 app.use((req, res, next) => {
-	if (
-		req.originalUrl.startsWith("/api") ||
-		req.originalUrl.startsWith("/signup") ||
-		req.originalUrl.startsWith("/signin")
-	) {
+	if (req.originalUrl.startsWith("/api")) {
+		// Si la ruta comienza con "/api", pasa al siguiente middleware
 		next();
 	} else {
-		// Construir la ruta absoluta al archivo index.html del frontend
-		const indexPath = path.join(
-			__dirname,
-			"../Vue_FrontendV2/dist",
-			"index.html",
-		);
-		res.sendFile(indexPath);
+		// Si la ruta no comienza con "/api", redirige al archivo de entrada del frontend
+		res.sendFile(path.join(__dirname, "../Vue_FrontendV2/dist", "index.html"));
 	}
-});
-app.use((req, res, next) => {
-	console.log(req.body);
-	bodyParser.json()(req, res, next);
 });
 
 // Rutas API
-app.use("/", routes);
+app.use("/api", routes);
 app.use((req, res, next) => {
 	res.setHeader("Strict-Transport-Security", "max-age=31536000;");
 	next();
 });
+// Middleware de manejo de errores
+app.use((err, req, res, next) => {
+	console.error(err.stack);
+	res.status(500).json({
+		error: "Error interno del servidor",
+		message: err.message,
+	});
+});
 
+app.use((req, res, next) => {
+	console.log(req.body); // Agrega este registro
+	bodyParser.json()(req, res, next);
+});
 // Conexión a la base de datos MongoDB
 mongoose
 	.connect(process.env.MONGODB_URI)
@@ -107,16 +94,13 @@ mongoose
 		console.error("Error al conectar a la base de datos:", error.message);
 	});
 
-// Middleware de manejo de errores
-app.use((err, req, res, next) => {
-	console.error(err.stack);
-	res.status(500).json({
-		error: "Error interno del servidor",
-		message: err.message,
-	});
-});
+// Configura el servidor HTTPS
+const httpsServer = https.createServer(credentials, app);
 
-// Crear el servidor HTTPS
-https.createServer(options, app).listen(port, () => {
-	console.log("El servidor está online"); // Inicia el servidor HTTPS en el puerto especificado
+// Define el puerto para el servidor HTTPS
+const httpsPort = process.env.HTTPS_PORT || 443;
+
+// Inicia el servidor HTTPS
+httpsServer.listen(httpsPort, () => {
+    console.log("Servidor HTTPS iniciado en el puerto " + httpsPort);
 });
