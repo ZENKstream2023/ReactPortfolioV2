ed1f58f2d1cfe3e433cb941580b4bb70e2a16646
ZENKstream2023 <zenkstreamhelp@gmail.com>
2024-04-29 17:35:02 +0200

    modified:   React_FrontendV2/index.html 	deleted:    React_FrontendV2/src/components/EditForm.jsx 	modified:   React_FrontendV2/src/components/footer.jsx

--- React_FrontendV2/src/components/footer.jsx
+++ React_FrontendV2/src/components/footer.jsx
@@ -19,7 +19,7 @@
 				<path d="M14.31 8l5.74 9.94M9.69 8h11.48M7.38 12l5.74-9.94M9.69 16L3.95 6.06M14.31 16H2.83m13.79-4l-5.74 9.94" />
 			</svg>
 			<small className="d-block mb-3 text-body-secondary">
-				&copy;ZENK_stream
+				&copy;Aritz Robledo Jorge
 			</small>
 		</div>
 
@@ -32,7 +32,7 @@
 			</ul>
 		</div>
 		<div className="col-6 col-md">
-			<h5>Nacidos en</h5>
+			<h5>Desde</h5>
 			<ul className="list-unstyled text-small">
 				<li>
 					<p className="link-secondary text-decoration-none">
