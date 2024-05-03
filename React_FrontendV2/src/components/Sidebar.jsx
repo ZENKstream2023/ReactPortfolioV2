105718991dc17effa9194c76a4021ab1b5f05ead
ZENKstream2023 <zenkstreamhelp@gmail.com>
2024-05-03 10:35:46 +0200

    new file:   React_FrontendV2/src/bbdd.json 	modified:   React_FrontendV2/src/components/Sidebar.jsx 	new file:   React_FrontendV2/src/components/bestsellers.jsx 	new file:   React_FrontendV2/src/components/totalsales.jsx 	modified:   React_FrontendV2/src/pages/Panel.jsx

--- React_FrontendV2/src/components/Sidebar.jsx
+++ React_FrontendV2/src/components/Sidebar.jsx
@@ -74,7 +74,7 @@ export const Sidebar = () => {
 					</svg>
 					<span className="fs-4 text-center">
 						<h2>
-							<b>GOD</b>MOD
+							<b>GLOGLO</b>
 						</h2>
 					</span>
 					<hr />
@@ -96,7 +96,7 @@ export const Sidebar = () => {
 									<path d="m7.646 9.354-3.792 3.792a.5.5 0 0 0 .353.854h7.586a.5.5 0 0 0 .354-.854L8.354 9.354a.5.5 0 0 0-.708 0" />
 									<path d="M11.414 11H14.5a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.5-.5h-13a.5.5 0 0 0-.5.5v7a.5.5 0 0 0 .5.5h3.086l-1 1H1.5A1.5 1.5 0 0 1 0 10.5v-7A1.5 1.5 0 0 1 1.5 2h13A1.5 1.5 0 0 1 16 3.5v7a1.5 1.5 0 0 1-1.5 1.5h-2.086z" />
 								</svg>
-								Streamers
+								Panel
 							</NavLink>
 						</li>
 						<li className="nav-item">
@@ -123,26 +123,6 @@ export const Sidebar = () => {
 							<NavLink
 								activeClassName="active-nav-link"
 								className="nav-link text-white d-flex align-items-center gap-2"
-								to="/colaborations"
-							>
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									width="16"
-									height="16"
-									fill="currentColor"
-									className="bi bi-twitch"
-									viewBox="0 0 16 16"
-								>
-									<path d="M3.857 0 1 2.857v10.286h3.429V16l2.857-2.857H9.57L14.714 8V0zm9.714 7.429-2.285 2.285H9l-2 2v-2H4.429V1.143h9.142z" />
-									<path d="M11.857 3.143h-1.143V6.57h1.143zm-3.143 0H7.571V6.57h1.143z" />
-								</svg>
-								Colaboraciones
-							</NavLink>
-						</li>
-						<li className="nav-item">
-							<NavLink
-								activeClassName="active-nav-link"
-								className="nav-link text-white d-flex align-items-center gap-2"
 								to="/moderation"
 							>
 								<svg
@@ -156,28 +136,7 @@ export const Sidebar = () => {
 									<path d="M5.338 1.59a61 61 0 0 0-2.837.856.48.48 0 0 0-.328.39c-.554 4.157.726 7.19 2.253 9.188a10.7 10.7 0 0 0 2.287 2.233c.346.244.652.42.893.533q.18.085.293.118a1 1 0 0 0 .101.025 1 1 0 0 0 .1-.025q.114-.034.294-.118c.24-.113.547-.29.893-.533a10.7 10.7 0 0 0 2.287-2.233c1.527-1.997 2.807-5.031 2.253-9.188a.48.48 0 0 0-.328-.39c-.651-.213-1.75-.56-2.837-.855C9.552 1.29 8.531 1.067 8 1.067c-.53 0-1.552.223-2.662.524zM5.072.56C6.157.265 7.31 0 8 0s1.843.265 2.928.56c1.11.3 2.229.655 2.887.87a1.54 1.54 0 0 1 1.044 1.262c.596 4.477-.787 7.795-2.465 9.99a11.8 11.8 0 0 1-2.517 2.453 7 7 0 0 1-1.048.625c-.28.132-.581.24-.829.24s-.548-.108-.829-.24a7 7 0 0 1-1.048-.625 11.8 11.8 0 0 1-2.517-2.453C1.928 10.487.545 7.169 1.141 2.692A1.54 1.54 0 0 1 2.185 1.43 63 63 0 0 1 5.072.56" />
 									<path d="M10.854 5.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 7.793l2.646-2.647a.5.5 0 0 1 .708 0" />
 								</svg>
-								Moderación
-							</NavLink>
-						</li>
-
-						<li className="nav-item">
-							<NavLink
-								activeClassName="active-nav-link"
-								className="nav-link text-white d-flex align-items-center gap-2"
-								to="/bot_hispi"
-							>
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									width="16"
-									height="16"
-									fill="currentColor"
-									className="bi bi-robot"
-									viewBox="0 0 16 16"
-								>
-									<path d="M6 12.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5M3 8.062C3 6.76 4.235 5.765 5.53 5.886a26.6 26.6 0 0 0 4.94 0C11.765 5.765 13 6.76 13 8.062v1.157a.93.93 0 0 1-.765.935c-.845.147-2.34.346-4.235.346s-3.39-.2-4.235-.346A.93.93 0 0 1 3 9.219zm4.542-.827a.25.25 0 0 0-.217.068l-.92.9a25 25 0 0 1-1.871-.183.25.25 0 0 0-.068.495c.55.076 1.232.149 2.02.193a.25.25 0 0 0 .189-.071l.754-.736.847 1.71a.25.25 0 0 0 .404.062l.932-.97a25 25 0 0 0 1.922-.188.25.25 0 0 0-.068-.495c-.538.074-1.207.145-1.98.189a.25.25 0 0 0-.166.076l-.754.785-.842-1.7a.25.25 0 0 0-.182-.135" />
-									<path d="M8.5 1.866a1 1 0 1 0-1 0V3h-2A4.5 4.5 0 0 0 1 7.5V8a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1v1a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2v-1a1 1 0 0 0 1-1V9a1 1 0 0 0-1-1v-.5A4.5 4.5 0 0 0 10.5 3h-2zM14 7.5V13a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V7.5A3.5 3.5 0 0 1 5.5 4h5A3.5 3.5 0 0 1 14 7.5" />
-								</svg>
-								Hispi
+								Permanencia
 							</NavLink>
 						</li>
 						<li className="nav-item">
@@ -197,7 +156,7 @@ export const Sidebar = () => {
 									<path d="M2 1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h9.586a2 2 0 0 1 1.414.586l2 2V2a1 1 0 0 0-1-1zm12-1a2 2 0 0 1 2 2v12.793a.5.5 0 0 1-.854.353l-2.853-2.853a1 1 0 0 0-.707-.293H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2z" />
 									<path d="M7.066 4.76A1.665 1.665 0 0 0 4 5.668a1.667 1.667 0 0 0 2.561 1.406c-.131.389-.375.804-.777 1.22a.417.417 0 1 0 .6.58c1.486-1.54 1.293-3.214.682-4.112zm4 0A1.665 1.665 0 0 0 8 5.668a1.667 1.667 0 0 0 2.561 1.406c-.131.389-.375.804-.777 1.22a.417.417 0 1 0 .6.58c1.486-1.54 1.293-3.214.682-4.112z" />
 								</svg>
-								Comandos
+								Atención
 							</NavLink>
 						</li>
 						<li className="nav-item">
@@ -216,45 +175,7 @@ export const Sidebar = () => {
 								>
 									<path d="M3.1.7a.5.5 0 0 1 .4-.2h9a.5.5 0 0 1 .4.2l2.976 3.974c.149.185.156.45.01.644L8.4 15.3a.5.5 0 0 1-.8 0L.1 5.3a.5.5 0 0 1 0-.6zm11.386 3.785-1.806-2.41-.776 2.413zm-3.633.004.961-2.989H4.186l.963 2.995zM5.47 5.495 8 13.366l2.532-7.876zm-1.371-.999-.78-2.422-1.818 2.425zM1.499 5.5l5.113 6.817-2.192-6.82zm7.889 6.817 5.123-6.83-2.928.002z" />
 								</svg>
-								Recompensas
-							</NavLink>
-						</li>
-						<li className="nav-item">
-							<NavLink
-								activeClassName="active-nav-link"
-								className="nav-link text-white d-flex align-items-center gap-2"
-								to="/raffle"
-							>
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									width="16"
-									height="16"
-									fill="currentColor"
-									className="bi bi-gift"
-									viewBox="0 0 16 16"
-								>
-									<path d="M3 2.5a2.5 2.5 0 0 1 5 0 2.5 2.5 0 0 1 5 0v.006c0 .07 0 .27-.038.494H15a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1v7.5a1.5 1.5 0 0 1-1.5 1.5h-11A1.5 1.5 0 0 1 1 14.5V7a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h2.038A3 3 0 0 1 3 2.506zm1.068.5H7v-.5a1.5 1.5 0 1 0-3 0c0 .085.002.274.045.43zM9 3h2.932l.023-.07c.043-.156.045-.345.045-.43a1.5 1.5 0 0 0-3 0zM1 4v2h6V4zm8 0v2h6V4zm5 3H9v8h4.5a.5.5 0 0 0 .5-.5zm-7 8V7H2v7.5a.5.5 0 0 0 .5.5z" />
-								</svg>
-								Sorteos
-							</NavLink>
-						</li>
-						<li className="nav-item">
-							<NavLink
-								activeClassName="active-nav-link"
-								className="nav-link text-white d-flex align-items-center gap-2"
-								to="/news"
-							>
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									width="16"
-									height="16"
-									fill="currentColor"
-									className="bi bi-lightning-charge"
-									viewBox="0 0 16 16"
-								>
-									<path d="M11.251.068a.5.5 0 0 1 .227.58L9.677 6.5H13a.5.5 0 0 1 .364.843l-8 8.5a.5.5 0 0 1-.842-.49L6.323 9.5H3a.5.5 0 0 1-.364-.843l8-8.5a.5.5 0 0 1 .615-.09zM4.157 8.5H7a.5.5 0 0 1 .478.647L6.11 13.59l5.732-6.09H9a.5.5 0 0 1-.478-.647L9.89 2.41z" />
-								</svg>
-								NEWS
+								Ofertas
 							</NavLink>
 						</li>
 						<li className="nav-item">
@@ -290,14 +211,7 @@ export const Sidebar = () => {
 							data-bs-toggle="dropdown"
 							aria-expanded="false"
 						>
-							<img
-								src={channelImageUrl}
-								alt=""
-								width="42"
-								height="42"
-								className="rounded-circle me-2"
-							/>
-							<strong>{channelName}</strong>
+							<strong>Perfil</strong>
 						</Dropdown.Toggle>
 						<Dropdown.Menu className="dropdown-menu dropdown-menu-dark text-small shadow">
 							<Dropdown.Item>
